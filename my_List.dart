e70be69455d22f93d450c4d1d51b47dd9766b67f
MLEE9999 <101166389+MLEE9999@users.noreply.github.com>
2022-11-30 00:24:07 +0900

    block_List연동
    block_List 제작에 따른 화면 이동 연결

--- my_List.dart
+++ my_List.dart
@@ -2,6 +2,7 @@ import 'package:application_20221022/chat_List.dart';
 import 'package:application_20221022/main.dart';
 import 'package:application_20221022/post_list.dart';
 import 'package:application_20221022/profile_edit.dart';
+import 'package:application_20221022/block_List.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:html/dom.dart' as dom;
@@ -29,7 +30,8 @@ class my_List extends StatelessWidget {
         '/friendList' : (context) => MyApp(), // MyApp 페이지로 값을 넘겨주기 위함
         '/chatList' : (context) => chat_List(), // chat_List 페이지로 값을 넘겨주기 위함
         '/postList' : (context) => post_List(), // post_List 페이지로 값을 넘겨주기 위함
-        '/profileEdit' : (context) => profile_edit()
+        '/profileEdit' : (context) => profile_edit(),
+        '/blockList' : (context) => block_List()
       },
       debugShowCheckedModeBanner: false,
       home: MyListPage(userEmail: usEmail.userEmail, userName: usEmail.userName, userStateMsg: usEmail.userStateMsg)
@@ -154,7 +156,7 @@ class _MyListPageState extends State<MyListPage> {
                   ),
                   child: TextButton( // 텍스트 버튼 위젯
                      onPressed: (){
-
+                       Navigator.pushNamed(context, '/blockList', arguments: BlockList_UserEmail(userEmail: widget.userEmail, userName: widget.userName, userStateMsg: widget.userStateMsg)); // 전체 목록 페이지로 이동 및 인자값 전달
                      },
                      child: Padding( // 여백을 주기 위해 사용하는 위젯
                        padding: EdgeInsets.fromLTRB(10, 0, 0, 0), // 왼 10 위 0 아래 0 우 0의 여백을 줌
