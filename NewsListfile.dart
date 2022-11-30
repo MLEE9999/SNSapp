b5f614367e4a05b84d55a85714657c13afd0645e
MLEE9999 <101166389+MLEE9999@users.noreply.github.com>
2022-12-01 00:19:48 +0900

    공지사항 리스트
    공지사항오픈시 가져올 객체

---
+++ NewsListfile.dart
@@ -0,0 +1,12 @@
+class NewsListfile{
+  final String LoginuserEmail;
+  final String LoginuserName;
+  final String LoginuserStateMsg;
+
+  final String newsUid;
+  final String newsTitle;
+  final String newsContents;
+  final String newsTime;
+
+  NewsListfile(this.LoginuserEmail, this.LoginuserName, this.LoginuserStateMsg, this.newsUid, this.newsTitle, this.newsContents, this.newsTime);
+}
