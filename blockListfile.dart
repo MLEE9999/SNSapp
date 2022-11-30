89778a3cc4c65b31c296ee5c65ba9bd1e5589c03
MLEE9999 <101166389+MLEE9999@users.noreply.github.com>
2022-11-30 14:52:51 +0900

    블랙리스트 파일
    블랙리스트 호출을 위한 클래스

---
+++ blockListfile.dart
@@ -0,0 +1,10 @@
+class blockListfile{
+  final String LoginuserEmail;
+  final String LoginuserName;
+  final String LoginuserStateMsg;
+
+  final String userEmail;
+  final String userName;
+
+  blockListfile(this.LoginuserEmail, this.LoginuserName, this.LoginuserStateMsg, this.userEmail, this.userName);
+}
